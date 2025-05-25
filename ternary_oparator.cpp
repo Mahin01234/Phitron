d79b6b64c73c155170e244b3fd88d5371dae4fbf
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2025-05-25 20:00:18 +0600

    Add files via upload

---
+++ ternary_oparator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ; 
+int main()
+{
+    int x = 10 ; 
+     
+    // if_else er short from Ternary oparator
+    // Rules : - 
+    // condition ? expression_if_true : expression_if_false;
+    x%2==0 ? cout << "Even"  :  cout << "Odd"  ; 
+
+
+    return 0 ;
+}
