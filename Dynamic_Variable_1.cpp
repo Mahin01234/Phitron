ab04bb53d011ddcffc962cd71d2394885429325b
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2025-05-26 15:41:18 +0600

    Add files via upload

---
+++ Dynamic_Variable_1.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+
+
+using namespace std ; 
+
+
+int main()
+{
+    int x = 10 ; 
+    int * p = new int ; 
+    * p = 1010 ; 
+
+
+    cout << *p << endl ; 
+
+    return 0 ; 
+}
