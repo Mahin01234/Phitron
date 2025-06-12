92c060070b0788d5f12a38191a830236160e1049
Mahinur Rahaman Mahin <taslima2422@gmail.com>
2025-06-12 12:23:12 +0600

    Add files via upload

---
+++ Sort_array_of_object_part_1.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std; 
+
+
+class Student 
+{
+
+    public :
+    string Name ; 
+    int Class ; 
+    int Roll ; 
+    int Number ; 
+
+} ; 
+
+
+
+
+
+bool cmp (Student l , Student r) 
+{
+    if(l.Number < r.Number) 
+    {
+        return true ; 
+    }
+    else
+    {
+        return false ; 
+    }
+}
+
+
+
+
+int main()
+{
+    int n ; 
+    cin >> n ; 
+    Student a[n] ; 
+
+
+
+    for(int i=0 ; i<n ; i++) 
+    {
+   
+        cin >> a[i].Name >> a[i].Class >> a[i].Roll >> a[i].Number ; 
+    }
+    
+
+    cout << endl ; 
+    sort(a, a+n, cmp) ; 
+    for(int i=0 ; i <n ; i++)
+    {
+        cout << a[i].Name << " " << a[i].Class << " " << a[i].Roll << " " << a[i].Number << endl ; 
+    }
+    return 0 ; 
+}
